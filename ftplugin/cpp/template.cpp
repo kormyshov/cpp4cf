0134086ba1a9d8666f4239f78298066370c4cfe4
Mikhail Kormyshov <kormyshov@gmail.com>
2012-10-10 18:48:20 +0800

    Add function LoadTemplate()

--- ftplugin/cpp/template.cpp
+++ ftplugin/cpp/template.cpp
@@ -1,23 +1,4 @@
-/* Test on
-2 3
-// 5
-*/
-
-/* Test on
-100 1
-// 101
-*/
-
-/* Test off
-0 0
-// 0
-*/
-
-/* Test on
--1 1
-// 0
-*/
-
+// [[TESTS]]
 #include <iostream>
 #include <fstream>
 #include <cstdio>
@@ -25,54 +6,10 @@
 
 using namespace std;
 
-int cpp4cf_main()
-{
-	int a, b;
-	cin>>a>>b;
-	cout<<a+b;
-	return 0;
-}
-
-
-// [[MAIN]]
 int main()
 {
-	freopen("template.cpp", "r", stdin);
-
-	char s[99];
-	bool f;
-
-	while(true) {
-		cin>>s;
-		if(cin.eof()) break;
-		if(strstr(s,"/*")){
-			cin>>s;
-			if(strstr(s,"Test")){
-				cin>>s;
-				if(strstr(s,"on")){
-					cout<<"Output: ";
-					cpp4cf_main();
-					cout<<"\nAnswer: ";
-					f = false;
-					while(true) {
-						cin>>s;
-						if(strstr(s,"*/")) break;
-						if(strcmp(s,"//") == 0){
-							if(f) cout<<endl;
-							else f = true;
-						}
-						else cout<<s<<" ";
-					}
-					cout<<"\n\n";
-				}
-			}
-		}
-	}
 
 	return 0;
 }
-// [[end cpp4cf]]
-/* Test on
-7 8
-// 15
-*/
+
+// [[MAIN]]
